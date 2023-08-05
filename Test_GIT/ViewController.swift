6de33fb685e3e0db4dc9aa28d32bf5496b21329e
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-08-05 20:49:38 +0500

    Initial Commit

---
+++ Test_GIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_GIT
+//
+//  Created by Ravshanbek Tursunbaev on 5.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
